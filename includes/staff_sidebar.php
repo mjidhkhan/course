1dcb373d91a72ec747cbaba5ea51c79d5c4373f8
Majid Hameed Khan <majidhkhan@outlook.com>
2019-04-17 20:20:12 +0100

    new changes

--- includes/staff_sidebar.php
+++ includes/staff_sidebar.php
@@ -3,8 +3,10 @@
             <ul id="accordion">
                 <li class="active">
                     <ul class="blocklist">
-                    <?php if($_SESSION['status'] ==1 || $_SESSION['status'] == 2){ ?>
-                        <?php if(false){ ?>
+                    <?php if ($_SESSION['status'] == 1 || $_SESSION['status'] == 2) {
+    ?>
+                        <?php if (false) {
+        ?>
                         <li class="second" style="border: 1px solid rgb(231, 8, 8)">
                         
                                 <p class="sidebar-title out-stock">Notifications</p>
@@ -13,25 +15,31 @@
                                 
                                 </ul>
                             </li>
-                        <?php }?>
-                   <?php } ?>
+                        <?php
+    } ?>
+                   <?php
+} ?>
                         <li class="second">
                             <p class="sidebar-title">Admin Area</p>
                             <ul>
-                                <?php if($_SESSION['status'] ==1){ ?>
+                                <?php if ($_SESSION['status'] == 1) {
+        ?>
                                 <li><a href="new_staff.php">New Staff</a></li>
                                 <li><a href="new_staff.php">Fire Staff</a></li>
-                                <?php } ?>
+                                <?php
+    } ?>
                             </ul>
                         </li>
                         <li class="second">
                             <p class="sidebar-title">Staff area</p>
                             <ul>
-                                <?php if($_SESSION['status'] ==1 || $_SESSION['status'] == 2){ ?>
+                                <?php if ($_SESSION['status'] == 1 || $_SESSION['status'] == 2) {
+        ?>
                                     <li><a href="new_dish.php"> New Recipe </a></li>
                                     <li><a href="view_orders.php">View orders</a></li>
-                                    <li><a href="view_orders.php">Stock check</a></li>
-                                <?php } ?>
+                                    <li><a href="rcp_plane.php">Stock check</a></li>
+                                <?php
+    } ?>
                             </ul>
                         </li>
                         
